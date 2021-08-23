ddd4046ab43f37feb281cb0d6602c5ffb8c7d2d9
rotarycut <shermanyong@gmail.com>
2021-08-23 21:30:28 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,20 +3,7 @@ import React from 'react';
 import MainForm from './components/MainForm';
 import './App.css';
 
-
 function App() {
-
-  // const [submitting, setSubmitting] = useState(false);
-  // const handleSubmit = event => {
-  //   event.preventDefault();
-  //  setSubmitting(true);
-
-  //  //this shld call the external api to submit to backend
-  //  setTimeout(() => {
-  //    setSubmitting(false);
-  //  }, 3000)
-  // }
- 
   return (
     <div>
       <MainForm/>
